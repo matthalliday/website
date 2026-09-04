1e0249b72982df3783fae4c3f37ce5ee282db51d
Matt Halliday <matthalliday@gmail.com>
2026-09-03 21:18:49 -0400

    Remove /ping endpoint

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,6 @@ export default {
     env: Env,
     _ctx: ExecutionContext,
   ): Promise<Response> {
-    const url = new URL(request.url);
-
-    if (url.pathname === "/ping") {
-      return new Response("pong", {
-        headers: {
-          "Cache-Control": "no-store, no-cache, must-revalidate",
-        },
-      });
-    }
-
     return env.ASSETS.fetch(request);
   },
 } satisfies ExportedHandler<Env>;
